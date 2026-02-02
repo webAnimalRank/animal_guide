6218c34a5f365f57598271f6e746faed43e0535a
seharuem <seharuem@naver.com>
2026-02-02 20:36:08 +0900

    주민 명부 상세정보 추가

--- src/pages/villager/Villager.jsx
+++ src/pages/villager/Villager.jsx
@@ -1,17 +1,49 @@
-import { CardWrap, Mini, Wrap2 } from './villager.style';
+import { Card, CardWrap, Close, List, Mini, Wrap2 } from './villager.style';
 import { char } from '../../temp-data/data';
+import { Box } from '../../components/style';
+import { useState } from 'react';
+
+const list = ['이름', '성별', '종족', '생일', '데뷔'];
 
 export default function Villager() {
-	return (
-		<Wrap2>
-			<CardWrap>
-				{char.map((v, i) => (
-					<Mini key={i}>
-						<img src={v.mini} alt='' />
-						{v.name}
-					</Mini>
-				))}
-			</CardWrap>
-		</Wrap2>
-	);
+  const [isSelect, setIsSelect] = useState(false);
+  const [data, setData] = useState();
+  const select = (i) => {
+    setIsSelect(true);
+    setData(i);
+  };
+
+  return (
+    <>
+      <Wrap2>
+        <CardWrap>
+          {char.map((v, i) => (
+            <Mini key={i} onClick={() => select(i)}>
+              <img src={v.mini} alt="" />
+              {v.name}
+            </Mini>
+          ))}
+        </CardWrap>
+      </Wrap2>
+      {isSelect && (
+        <div className="fixed inset-0 bg-black/30 flex flex-col gap-8 max-sm:gap-5 items-center justify-center max-sm:pt-20">
+          <Card>
+            <Box className="h-full rounded-4xl max-sm:rounded-3xl p-6 max-sm:p-4 w-max self-center">
+              <img className="h-full object-contain max-md:h-50 max-sm:h-30" src={char[data].img} alt="" />
+            </Box>
+
+            <ul className="flex flex-col justify-between max-md:gap-5 max-sm:gap-3">
+              {list.map((l, i) => (
+                <li key={i} className="flex gap-5 items-center text-xl max-sm:text-base ">
+                  <List>{l}</List>
+                  너굴 | test | 너굴
+                </li>
+              ))}
+            </ul>
+          </Card>
+          <Close onClick={() => setIsSelect(false)}>닫기</Close>
+        </div>
+      )}
+    </>
+  );
 }
--- src/pages/villager/villager.style.js
+++ src/pages/villager/villager.style.js
@@ -1,5 +1,7 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { Wrap } from '../../components/style';
+
+const base = '/animal_guide';
 
 export const Wrap2 = styled(Wrap).attrs({
   className: 'pr-2 max-sm:pr-0'
@@ -47,4 +49,54 @@ export const Mini = styled.button.attrs({
   &:hover {
     background-color: rgba(255, 255, 255, 0.4);
   }
+`;
+
+const fade = keyframes`
+to { opacity: 1 }
+`;
+
+export const Card = styled.div.attrs({
+  className:
+    'flex flex-col h-90 aspect-5/3 p-10 max-sm:p-5 rounded-4xl flex-row bg-(--bg) gap-8 max-sm:gap-5 backdrop-blur-xl font-extrabold opacity-0 max-md:h-auto max-md:w-120 max-md:flex-col max-md:aspect-auto max-sm:w-100 text-shadow-(--shadowW)'
+})`
+  border: 1px solid var(--p);
+  background: linear-gradient(
+    135deg,
+    #8bb363,
+    rgba(255, 255, 255, 0.7),
+    #8bb363
+  );
+  box-shadow: 0 0 4px var(--c);
+  animation: ${fade} 0.2s ease-out forwards;
+  @media (max-width: 480px) {
+    width: 20rem;
+    padding: 1rem;
+  }
+`;
+
+export const List = styled.span.attrs({
+  className:
+    'bg-(image:--glass) rounded-full py-2 px-4 text-lg max-sm:text-sm flex gap-2 text-(--c)'
+})`
+  &::before {
+    content: '';
+    width: 1rem;
+    aspect-ratio: 1;
+    mask: url(${base}/link2.svg) center / contain no-repeat;
+    background-color: var(--p);
+  }
+`;
+
+export const Close = styled.button.attrs({
+  type: 'button',
+  className:
+    'bg-(image:--glass) rounded-full px-4 py-2 backdrop-blur-3xl font-bold opacity-0'
+})`
+  box-shadow: 0 0 4px var(--c);
+  transition: background-color 0.1s ease-out;
+  background-color: rgba(255, 255, 255, 0.5);
+  &:hover {
+    background-color: rgba(255, 255, 255, 0.8);
+  }
+  animation: ${fade} 0.2s ease-out forwards;
 `;
