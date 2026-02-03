9874c9a4f87a47a24f9e5e7c51b57df661b582a6
seharuem <seharuem@naver.com>
2026-02-03 10:46:05 +0900

    이메일 필드 추가

--- src/pages/sign/Sign.jsx
+++ src/pages/sign/Sign.jsx
@@ -19,6 +19,9 @@ export default function Sign() {
 					<input type='text' name='id' placeholder='아이디' />
 				</Field>
 				<Field>
+					<input type='email' name='email' placeholder='이메일' />
+				</Field>
+				<Field>
 					<input type='password' name='pw' placeholder='비밀번호' />
 				</Field>
 				<Btn onClick={signUp}>회원 가입</Btn>
