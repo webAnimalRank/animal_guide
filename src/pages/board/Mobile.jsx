709da13b43921c08db7ad06d40724e05c80889d4
Parkhunsang <daxi99@naver.com>
2026-02-23 17:58:38 +0900

    게시판 글 조회 기능 추가

--- src/pages/board/Mobile.jsx
+++ src/pages/board/Mobile.jsx
@@ -4,7 +4,7 @@ export default function Mobile({ table }) {
 	return (
 		<div className='w-full flex flex-col sm:hidden border-t-2 border-(--c)'>
 			{table.getRowModel().rows.map((row) => (
-				<Line to='post' key={row.id} className='w-full flex-col gap-2'>
+				<Line to={`post/${row.original.id}`} key={row.id} className='w-full flex-col gap-2'>
 					<span className='w-full text-sm text-left font-bold'>{row.original.title}</span>
 					<div className='flex gap-2 text-xs self-end'>
 						{row.original.writer} | {row.original.createdAt}
