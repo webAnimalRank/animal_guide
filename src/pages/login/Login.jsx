38768ea27f43901250054657e1c90c0d2c0c2a7b
seharuem <seharuem@naver.com>
2026-04-09 01:12:38 +0900

    다중클릭 방지

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -9,49 +9,61 @@ import toast from 'react-hot-toast';
 const API_URL = import.meta.env.VITE_API_BASE_URL;
 
 export default function Login() {
-	const { setMember } = useFetchStore();
+  const { setMember } = useFetchStore();
 
-	const navigate = useNavigate();
-	const [id, setId] = useState('');
-	const [pw, setPw] = useState('');
+  const navigate = useNavigate();
+  const [id, setId] = useState('');
+  const [pw, setPw] = useState('');
 
-	const logIn = async (e) => {
-		e.preventDefault();
-		console.log('로그인 시도 : ', { id, pw });
-		try {
-			await loginMember({ memberId: id, memberPw: pw });
+  const logIn = async (e) => {
+    e.preventDefault();
+    console.log('로그인 시도 : ', { id, pw });
+    try {
+      await loginMember({ memberId: id, memberPw: pw });
 
-			// 로그인 후 바로 사용자 정보 가져오기
-			const res = await axios.get(`${API_URL}/api/members/me`, {
-				withCredentials: true
-			});
-			setMember(res.data); // App의 전역 상태 업데이트
+      // 로그인 후 바로 사용자 정보 가져오기
+      const res = await axios.get(`${API_URL}/api/members/me`, {
+        withCredentials: true
+      });
+      setMember(res.data); // App의 전역 상태 업데이트
 
-			if (res.status === 200) {
-				localStorage.setItem('isLogin', 'true');
-			}
+      if (res.status === 200) {
+        localStorage.setItem('isLogin', 'true');
+      }
 
-			toast.success(`${res.data.memberName}님 환영합니다!`);
-			navigate('/');
-		} catch (err) {
-			console.error('로그인 실패:', err);
-			toast.error('아이디 혹은 비밀번호를 확인해주세요.');
-		}
-	};
+      toast.success(`${res.data.memberName}님 환영합니다!`);
+      navigate('/');
+    } catch (err) {
+      console.error('로그인 실패:', err);
+      toast.error('아이디 혹은 비밀번호를 확인해주세요.', { id: 'loginErr' });
+    }
+  };
 
-	return (
-		<>
-			<h2 className='font-extrabold text-2xl'>로그인</h2>
-			<Form onSubmit={logIn}>
-				<Field>
-					<input type='text' name='id' placeholder='아이디' value={id} onChange={(e) => setId(e.target.value)} />
-				</Field>
-				<Field>
-					<input type='password' name='pw' placeholder='비밀번호' value={pw} onChange={(e) => setPw(e.target.value)} />
-				</Field>
-				<Btn>로그인</Btn>
-				<Btn2 to='/sign'>회원 가입</Btn2>
-			</Form>
-		</>
-	);
+  return (
+    <>
+      <h2 className="font-extrabold text-2xl">로그인</h2>
+      <Form onSubmit={logIn}>
+        <Field>
+          <input
+            type="text"
+            name="id"
+            placeholder="아이디"
+            value={id}
+            onChange={(e) => setId(e.target.value)}
+          />
+        </Field>
+        <Field>
+          <input
+            type="password"
+            name="pw"
+            placeholder="비밀번호"
+            value={pw}
+            onChange={(e) => setPw(e.target.value)}
+          />
+        </Field>
+        <Btn>로그인</Btn>
+        <Btn2 to="/sign">회원 가입</Btn2>
+      </Form>
+    </>
+  );
 }
