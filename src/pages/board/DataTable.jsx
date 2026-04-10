d379766121b8336e13dc13e114be13905213e87c
seharuem <seharuem@naver.com>
2026-04-10 23:01:54 +0900

    검색결과 없을때 안내문구 추가

--- src/pages/board/DataTable.jsx
+++ src/pages/board/DataTable.jsx
@@ -5,11 +5,12 @@ import { useBoardStore } from './useStore';
 import { useEffect } from 'react';
 import { columns } from './columns';
 import Pagination from './Pagination';
+import { Loading } from '../../components/style';
 
 // 컬럼 정의
 // React Table은 columns가 반드시 필요함
 export default function DataTable({ kind }) {
-	const { items, meta, page, size, loading } = useBoardStore((state) => state[kind]);
+	const { items, meta, page, size, loading, keyword } = useBoardStore((state) => state[kind]);
 	const fetchPosts = useBoardStore((state) => state.fetchPosts);
 
 	useEffect(() => {
@@ -31,8 +32,20 @@ export default function DataTable({ kind }) {
 		meta: { kind: kind }
 	});
 
-	if (loading && (!items || items.length === 0)) {
-		return <div>데이터를 불러오는 중입니다...</div>;
+	if (loading) {
+		return (
+			<div className='py-10 bg-white/10 rounded-xl flex justify-center items-center'>
+				<Loading className='size-10' />
+			</div>
+		);
+	}
+
+	if (!items || items.length === 0) {
+		return (
+			<div className='py-10 bg-white/10 rounded-xl'>
+				{keyword ? `"${keyword}" 에 대한 검색 결과가 없습니다.` : '등록된 게시물이 없습니다.'}
+			</div>
+		);
 	}
 
 	return (
