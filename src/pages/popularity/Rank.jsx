bdc35ea2c6e05de0b6818cb8e0440508cf328921
seharuem <seharuem@naver.com>
2026-04-30 17:35:06 +0900

    ui 수정

--- src/pages/popularity/Rank.jsx
+++ src/pages/popularity/Rank.jsx
@@ -12,61 +12,68 @@ export default function Rank() {
   }, [fetchRanking]);
 
   return (
-    <div className='w-full'>
-      <div className='font-bold text-xl mb-4'>
+    <div className='w-full min-h-0 flex-1 flex gap-4 flex-col items-center'>
+      <div className='font-bold text-xl'>
         {rankingMonth ? `${rankingMonth} 투표 현황` : '이번 달 투표 현황'}
       </div>
-      <table className='bg-white/10 w-full'>
-        <thead>
-          <tr>
-            <th>순위</th>
-            <th className='w-30 p-4'>이미지</th>
-            <th>이름</th>
-            <th>표</th>
-          </tr>
-        </thead>
-        <tbody>
-          {rankingLoading && (
-            <tr>
-              <td colSpan='4' className='p-8 text-center'>
-                <div className='flex justify-center'>
-                  <Loading className='size-10' />
-                </div>
-              </td>
-            </tr>
-          )}
-          {!rankingLoading && rankingError && (
-            <tr>
-              <td colSpan='4' className='p-6 text-center text-red-500 font-bold'>
-                투표 현황을 불러오지 못했습니다.
-              </td>
-            </tr>
-          )}
-          {!rankingLoading && !rankingError && ranking.length === 0 && (
-            <tr>
-              <td colSpan='4' className='p-6 text-center font-bold'>
-                아직 집계된 투표가 없습니다.
-              </td>
-            </tr>
-          )}
-          {!rankingLoading &&
-            !rankingError &&
-            ranking.map((item) => (
-              <tr key={item.villagerNo}>
-                <td className='text-center'>{item.rank}</td>
-                <td className='p-4'>
-                  <img
-                    className='w-30 mx-auto'
-                    src={item.villagerImage || item.villagerImageIcon || tom}
-                    alt={item.villagerName}
-                  />
-                </td>
-                <td className='text-center'>{item.villagerName}</td>
-                <td className='text-center'>{item.votes}표</td>
+      <div className='w-max min-h-0 flex-1 bg-white/20 rounded-xl overflow-hidden'>
+        <div className='h-full overflow-y-scroll'>
+          <table className='relative'>
+            <thead className='sticky top-0 bg-(--cw)'>
+              <tr>
+                <th className='w-20'>순위</th>
+                <th className='w-30 p-2'>이미지</th>
+                <th className='w-50'>이름</th>
+                <th className='w-20'>표</th>
               </tr>
-            ))}
-        </tbody>
-      </table>
+            </thead>
+            <tbody>
+              {rankingLoading && (
+                <tr>
+                  <td colSpan='4' className='p-8 text-center'>
+                    <div className='flex justify-center'>
+                      <Loading className='size-10' />
+                    </div>
+                  </td>
+                </tr>
+              )}
+              {!rankingLoading && rankingError && (
+                <tr>
+                  <td
+                    colSpan='4'
+                    className='p-6 text-center text-red-500 font-bold'
+                  >
+                    투표 현황을 불러오지 못했습니다.
+                  </td>
+                </tr>
+              )}
+              {!rankingLoading && !rankingError && ranking.length === 0 && (
+                <tr>
+                  <td colSpan='4' className='p-6 text-center font-bold'>
+                    아직 집계된 투표가 없습니다.
+                  </td>
+                </tr>
+              )}
+              {!rankingLoading &&
+                !rankingError &&
+                ranking.map((item) => (
+                  <tr key={item.villagerNo} className='border-b-2 border-(--c)/30'>
+                    <td className='text-center'>{item.rank}</td>
+                    <td className='p-4'>
+                      <img
+                        className='w-30 mx-auto'
+                        src={item.villagerImageIcon || tom}
+                        alt={item.villagerName}
+                      />
+                    </td>
+                    <td className='text-center'>{item.villagerName}</td>
+                    <td className='text-center'>{item.votes}표</td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
